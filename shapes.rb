effeea1020c6a348cc83deec4497b6f8e3679b60
Jakub Kaláb <jakubkalab@gmail.com>
2019-10-31 12:36:47 +0100

    added support for small note numbers
    Sudoku::UI is now fully scalable
    - all sizes are computed based on `board_size` and `margin` params
    - `margin` itself is computed from `board_size` if not given
    
    refactored shapes
    - renamed `CustomShapes` to `Shapes`
    - added `Shapes::Line` - directly references `Ruby2D::Line`
    - replaced `TextUtils` by `Shapes::Text`
    - base `Grid` class no longer draws border lines
      - instead the initializer takes a block which is called for each cell,
        passing a `Cell` instance
      - the base `Grid` class can either be used directly for custom logic
        (e.g. a grid with nested grids in each cell)
        or one can use the `GridWithBorders` or `GridWithChars` subclasses
        which provide a the block for the base initializer
    
    refactored `Sudoku::UI`
    - added a ton of helper variables for computing positions and sizes
    - extracted several helper methods

--- shapes.rb
+++ shapes.rb
@@ -1,29 +1,42 @@
 require 'ruby2d'
-require_relative 'utils'
 
-module CustomShapes
-  class Square
-    attr_reader :x, :y, :size, :width, :color
+module Shapes
+  Line = Ruby2D::Line
 
-    def initialize(x:, y:, size:, width: 1, color: 'white')
-      @x      = x
-      @y      = y
-      @size   = size
-      @width  = width
-      @color  = color
+  class Text < SimpleDelegator
+    DEFAULT_FONT       = 'DejaVuSansMono.ttf'
+    DEFAULT_FONT_SIZE  = 12
+    DEFAULT_FONT_COLOR = 'white'
 
-      @left   = Ruby2D::Line.new(x1: x, y1: y,
-                                 x2: x, y2: y + size,
-                                 width: width, color: color)
-      @right  = Ruby2D::Line.new(x1: x + size, y1: y,
-                                 x2: x + size, y2: y + size,
-                                 width: width, color: color)
-      @top    = Ruby2D::Line.new(x1: x, y1: y,
-                                 x2: x + size, y2: y,
-                                 width: width, color: color)
-      @bottom = Ruby2D::Line.new(x1: x, y1: y + size,
-                                 x2: x + size, y2: y + size,
-                                 width: width, color: color)
+    def initialize(text, x:, y:,
+                   font: DEFAULT_FONT, size: DEFAULT_FONT_SIZE, color: DEFAULT_FONT_COLOR)
+      super(Ruby2D::Text.new(text, font: font, size: size, color: color, x: x, y: y))
+    end
+  end
+
+  class Rectangle
+    attr_reader :x, :y, :width, :height, :border_width, :border_color
+
+    def initialize(x:, y:, width:, height:, border_width: 1, border_color: 'white')
+      @x            = x
+      @y            = y
+      @width        = width
+      @height       = height
+      @border_width = border_width
+      @border_color = border_color
+
+      @left   = Line.new(x1: x, y1: y,
+                         x2: x, y2: y + height,
+                         width: border_width, color: border_color)
+      @right  = Line.new(x1: x + width, y1: y,
+                         x2: x + width, y2: y + height,
+                         width: border_width, color: border_color)
+      @top    = Line.new(x1: x, y1: y,
+                         x2: x + width, y2: y,
+                         width: border_width, color: border_color)
+      @bottom = Line.new(x1: x, y1: y + height,
+                         x2: x + width, y2: y + height,
+                         width: border_width, color: border_color)
     end
 
     def add
@@ -35,12 +48,18 @@ module CustomShapes
     end
 
     def contains?(coords)
-      @x <= coords.x && coords.x <= @x + @size && @y <= coords.y && coords.y <= @y + @size
+      @x <= coords.x && coords.x <= @x + @width && @y <= coords.y && coords.y <= @y + @height
+    end
+  end
+
+  class Square < Rectangle
+    def initialize(size:, **kwargs)
+      super(width: size, height: size, **kwargs)
     end
   end
 
   class Grid
-    Cell = Struct.new(:row, :column) do
+    Cell = Struct.new(:row, :column, :x, :y, :width, :height, keyword_init: true) do
       def inspect
         "#<#{self.class.name} row=#{row} column=#{column}>"
       end
@@ -52,72 +71,59 @@ module CustomShapes
 
     attr_reader :cell_height, :cell_width
 
-    def initialize(x:, y:, size:, rows:, columns:, width: 1, color: 'white')
-      @x       = x
-      @y       = y
-      @size    = size
-      @rows    = rows
-      @columns = columns
-      @width   = width
-      @color   = color
+    def initialize(x:, y:, width:, height:, rows:, columns:, &cell_block)
+      @x            = x
+      @y            = y
+      @width        = width
+      @height       = height
+      @rows         = rows
+      @columns      = columns
+      @cell_height  = height.to_f / rows
+      @cell_width   = width.to_f  / columns
 
-      @cell_height = size.to_f / rows
-      @cell_width  = size.to_f / columns
-
-      @lines = (0...rows).map do |row|
-        Ruby2D::Line.new(x1: x, y1: y + row * cell_height,
-                         x2: x + size, y2: y + row * cell_height,
-                         width: width, color: color)
-      end
-
-      @lines += (0...columns).map do |column|
-        Ruby2D::Line.new(x1: x + column * cell_width, y1: y,
-                         x2: x + column * cell_width, y2: y + size,
-                         width: width, color: color)
+      @children = (0...@rows).flat_map do |row|
+        (0...@columns).map do |column|
+          cell_block.call(Cell.new(row: row + 1, column: column + 1,
+                                   x: @x + column * @cell_width, y: @y + row * @cell_height,
+                                   width: @cell_width, height: @cell_height))
+        end
       end
     end
 
     def add
-      @lines.each(&:add)
+      @children.each(&:add)
     end
 
     def remove
-      @lines.each(&:remove)
+      @children.each(&:remove)
     end
 
     def cell_for(coords)
       row    = ((coords.y - @y) / @cell_height + 1).to_i
       column = ((coords.x - @x) / @cell_width  + 1).to_i
 
-      Cell.new(row, column)
+      Cell.new(row: row, column: column)
+    end
+  end
+
+  class GridWithBorders < Grid
+    def initialize(border_width: 1, border_color: 'white', **kwargs)
+      super(**kwargs) do |cell|
+        Rectangle.new(x: cell.x, y: cell.y,
+                      width: cell.width, height: cell.height,
+                      border_width: border_width, border_color: border_color)
+      end
     end
   end
 
   class GridWithChars < Grid
     def initialize(chars_matrix:, **kwargs)
-      super(**kwargs)
-
-      @chars_matrix = chars_matrix
-      @char_texts   = @chars_matrix.each_with_index.flat_map do |row_chars, row_index|
-        row_chars.each_with_index.map do |col_char, col_index|
-          TextUtils.draw_text(col_char,
-                              x: @x + col_index * @cell_width  + 1.0/4  * @cell_width,
-                              y: @y + row_index * @cell_height + 1.0/10 * @cell_height,
-                              size: 3.0/4 * @cell_height)
-        end
+      super(**kwargs) do |cell|
+        Text.new(chars_matrix[cell.row - 1][cell.column - 1],
+                            x: cell.x + 1.0/3.5 * cell.width,
+                            y: cell.y + 1.0/12  * cell.height,
+                            size: 3.0/4 * cell.height)
       end
-    end
-
-    def add
-      super
-
-      @char_texts.each(&:add)
-    end
-
-    def remove
-      super
-
-      @char_texts.each(&:remove)
     end
   end
 end
--- sudoku.rb
+++ sudoku.rb
@@ -1,11 +1,16 @@
 require 'ruby2d'
-require_relative 'utils'
 require_relative 'shapes'
+
+# TODO: implement keyboard interaction
 
 module Sudoku
   class UI
     class Output
-      def initialize(widget:)
+      def self.for(widget:)
+        new(widget)
+      end
+
+      def initialize(widget)
         @widget = widget
       end
 
@@ -22,32 +27,60 @@ module Sudoku
 
 
     class Board
-      attr_reader :x, :y, :size
+      attr_reader :x, :y, :width, :height
 
-      def initialize(x:, y:, size:, output: $stdout)
+      def initialize(matrix:, x:, y:, width:, height:, border_color: 'white', output: $stdout)
+        @matrix = matrix
         @x      = x
         @y      = y
-        @size   = size
+        @width  = width
+        @height = height
         @output = output
 
-        numbers = (1..9).to_a
-        @matrix = numbers.map {|n| numbers.rotate(n-1)}
+        common_params = {
+          x: x, y: y,
+          width: width, height: height,
+        }
 
-        @main_border   = CustomShapes::Square.new(
-          x: x, y: y, size: size,                      width: 5, color: 'green')
-        @cells_grid    = CustomShapes::GridWithChars.new(
-          x: x, y: y, size: size, rows: 9, columns: 9, width: 1, color: 'green',
+        @main_border = Shapes::Rectangle.new(
+          **common_params,
+          border_width: 5, border_color: border_color)
+
+        @section_borders_grid = Shapes::GridWithBorders.new(
+          **common_params,
+          rows: 3, columns: 3,
+          border_width: 3, border_color: border_color)
+
+        @cell_borders_grid = Shapes::GridWithBorders.new(
+          **common_params,
+          rows: 9, columns: 9,
+          border_width: 1, border_color: border_color)
+
+        @final_numbers_grid = Shapes::GridWithChars.new(
+          **common_params,
+          rows: 9, columns: 9,
           chars_matrix: @matrix)
-        @sections_grid = CustomShapes::Grid.new(
-          x: x, y: y, size: size, rows: 3, columns: 3, width: 3, color: 'green')
+
+        full_matrix  = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        empty_matrix = [[], [], []]
+
+        @note_numbers_grid = Shapes::Grid.new(**common_params, rows: 9, columns: 9) do |cell|
+          Shapes::GridWithChars.new(x: cell.x + 1.0/20 * cell.width, y: cell.y + 1.0/20 * cell.height,
+                                    width: 9.0/10 * cell.width, height: 9.0/10 * cell.height,
+                                    rows: 3, columns: 3,
+                                    chars_matrix: (cell.row + cell.column).odd? ? full_matrix : empty_matrix)
+        end
+
+        @all_shapes = [@main_border, @section_borders_grid, @cell_borders_grid,
+                       @final_numbers_grid, @note_numbers_grid]
       end
 
       def add
-        [@main_border, @cells_grid, @sections_grid].each(&:add)
+        @all_shapes.each(&:add)
       end
 
       def remove
-        [@main_border, @cells_grid, @sections_grid].each(&:remove)
+        @all_shapes.each(&:remove)
       end
 
       def contains?(coords)
@@ -55,23 +88,33 @@ module Sudoku
       end
 
       def handle_event(event)
-        cell = @cells_grid.cell_for(event)
+        cell = @cell_borders_grid.cell_for(event)
 
         @output << "you clicked #{cell}"
       end
     end
 
+    DEFAULT_BOARD_SIZE = 600
 
-    def initialize
+    def initialize(board_size: DEFAULT_BOARD_SIZE, margin: board_size / 15)
+      full_spacer   = margin / 5
+      half_spacer   = margin / 10
+      font_size     = full_spacer * 1.5
+      texts_height  = 2 * font_size + 3 * full_spacer
+      window_width  = board_size + 2 * margin
+      window_height = board_size + 2 * margin + texts_height
+
       @tick         = 0
-      @window       = Ruby2D::Window.new
-      @info_text    = TextUtils.draw_text(get_info_text, y: 10)
-      @event_text   = TextUtils.draw_text(''           , y: @info_text.y + TextUtils::DEFAULT_FONT_SIZE + 5)
-      @info_output  = Output.new(widget: @info_text)
-      @event_output = Output.new(widget: @event_text)
-      @board        = Board.new(x: 50, y: 50, size: 600, output: @event_output)
-
-      @window.set(title: 'sudoku', width: @board.size + 2 * @board.x, height: @board.size + 2 * @board.y)
+      @window       = Ruby2D::Window.new(title: 'sudoku', width: window_width, height: window_height)
+      @info_text    = Shapes::Text.new(get_info_text, x: full_spacer, y: full_spacer,
+                                       size: font_size, color: 'green')
+      @event_text   = Shapes::Text.new('', x: full_spacer,
+                                       y: @info_text.y + @info_text.size + half_spacer,
+                                       size: font_size, color: 'green')
+      @info_output  = Output.for(widget: @info_text)
+      @event_output = Output.for(widget: @event_text)
+      @separator    = horizontal_separator(position: texts_height, length: window_width)
+      @board        = board(x: margin, y: texts_height + margin, size: board_size)
     end
 
     def show
@@ -81,6 +124,24 @@ module Sudoku
     end
 
     private
+
+    def board(x:, y:, size:)
+      numbers            = (1..9).to_a
+      empty_matrix       = (0..8).map {|i| numbers.map {nil}}
+      full_matrix        = (0..8).map {|i| numbers.rotate(i-1)}
+      alternating_matrix = (0..8).map do |i|
+        numbers.rotate(i).each_with_index.map {|num, j| num if (i + j).even?}
+      end
+
+      Board.new(matrix: alternating_matrix,
+                x: x, y: y,
+                width: size, height: size,
+                border_color: 'green', output: @event_output)
+    end
+
+    def horizontal_separator(position:, length:, offset: 0, width: 1, color: 'green')
+      Shapes::Line.new(x1: offset, y1: position, x2: offset + length, y2: position, width: width, color: color)
+    end
 
     def get_info_text
       "time: #{Time.now.strftime('%T')}, tick: #{@tick}"
